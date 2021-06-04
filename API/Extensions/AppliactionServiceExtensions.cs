f4a36becc194d0521dd23ca683f2670b76ed3dcf
alexkoshmelev <alexkoshmelev@rambler.ru>
2021-06-04 09:29:02 +0700

    End of Section 8

--- API/Extensions/AppliactionServiceExtensions.cs
+++ API/Extensions/AppliactionServiceExtensions.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
@@ -12,6 +13,10 @@ namespace API.Extensions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
             services.AddScoped<ITokenService, TokenService>();
+
+            services.AddScoped<IUserRepository,UserRepositoty>();
+
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
 
             services.AddDbContext<DataContext>(options =>
             {
